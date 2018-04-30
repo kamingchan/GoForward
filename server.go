38262c9b895d766c0b14990d5e6415b763e1cde9
Terry Chan <terry@32ph.com>
2018-04-30 14:44:18 +0800

    imp forward server

---
+++ server.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"net"
+	"fmt"
+)
+
+type Server struct {
+	localAddress    string
+	destAddress     string
+	tcpListener     net.Listener
+	udpListener     net.PacketConn
+	udpClients      map[string]*udpConnection
+	udpClientClosed chan string
+}
+
+func NewServer(l string, d string) *Server {
+	return &Server{
+		localAddress: l,
+		destAddress:  d,
+		udpClients:   make(map[string]*udpConnection),
+	}
+}
+
+func (s *Server) Start() {
+	// TCP forward
+	go func() error {
+		var l, err = net.Listen("tcp", s.localAddress)
+		if err != nil {
+			// TODO: Log
+			return err
+		}
+		s.tcpListener = l
+		for {
+			connection, err := s.tcpListener.Accept()
+			if err != nil {
+				return err
+			}
+
+			p := &tcpConnection{
+				localConnection: connection,
+				localAddress:    s.localAddress,
+				remoteAddress:   s.destAddress,
+			}
+
+			go p.forward()
+		}
+	}()
+	// UDP relay
+	go func() error {
+		var l, err = net.ListenPacket("udp", s.localAddress)
+		if err != nil {
+			// TODO: Log
+			return err
+		}
+		raddr, err := net.ResolveUDPAddr("udp", s.destAddress)
+		if err != nil {
+			return err
+		}
+		s.udpListener = l
+		s.udpClientClosed = make(chan string)
+
+		// handle closed udp connection
+		go func() {
+			for {
+				clientAddr := <-s.udpClientClosed
+
+				conn, found := s.udpClients[clientAddr]
+				if found {
+					fmt.Printf("timeout UDP connection from %s via %s\n", clientAddr, conn.remoteAddress.String())
+					conn.remoteConnection.Close()
+					delete(s.udpClients, clientAddr)
+				}
+			}
+		}()
+
+		for {
+			buf := make([]byte, 4096)
+			size, address, err := s.udpListener.ReadFrom(buf)
+			if err != nil {
+				s.udpListener.Close()
+			}
+
+			connection, found := s.udpClients[address.String()]
+			if !found {
+				remoteUDPConn, err := net.DialUDP("udp", nil, raddr)
+				if err != nil {
+					return err
+				}
+
+				connection = &udpConnection{
+					localConnection:  s.udpListener,
+					localAddress:     address,
+					remoteConnection: remoteUDPConn,
+					remoteAddress:    remoteUDPConn.LocalAddr(),
+					closeChan:        s.udpClientClosed,
+				}
+
+				s.udpClients[address.String()] = connection
+
+				fmt.Printf("new UDP connection from %s via %s\n", address.String(), remoteUDPConn.LocalAddr().String())
+
+				// wait for data from remote server
+				go connection.Reply()
+			}
+
+			go func() {
+				// forward data received to remote server
+				_, err = connection.remoteConnection.Write(buf[0:size])
+				if err != nil {
+					return
+				}
+			}()
+		}
+	}()
+}
